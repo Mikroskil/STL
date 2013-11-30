6bd843a906111acecfdc140766fb1e3781f190f6
mytosin <mytosinz@yahoo.com>
2013-12-01 00:46:44 +0700

    penambahan bootstrap dan pengubahan index.php untuk mengetes bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <link rel="stylesheet/less" type="text/css" href="styles.less" />
+    <link rel="stylesheet" href="bootstrap.css">
 	<script src="less-1.5.0.min.js" type="text/javascript"></script>
 	<script src="jquery-1.10.2.js"></script>
     <title>Jadwal</title>
@@ -12,6 +13,8 @@
 	<div id="center">
     	<article>
         	<h2>This is Article</h2>
+            <p>This is Paragraph</p>
+            <input type="button" class="btn" value="Login"/>
         </article>
         <section>
         	<h2>This is Section</h2>
