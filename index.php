03562560a72d38e98b1694ac5f53fad89e7ef361
mytosin <mytosinz@yahoo.com>
2013-11-30 21:27:58 +0700

    penambahan header dan import less dan jquery

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <html>
 <head>
-	<title>Jadwal</title>
+    <link rel="stylesheet/less" type="text/css" href="styles.less" />
+	<script src="less-1.5.0.min.js" type="text/javascript"></script>
+	<script src="jquery-1.10.2.js"></script>
+    <title>Jadwal</title>
 </head>
 <body>
-	
+	<header>
+    	
+    </header>
 </body>
 </html>
