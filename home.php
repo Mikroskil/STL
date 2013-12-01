a867bae931bd851094d8418633c1fc8e69715de9
mytosin <mytosinz@yahoo.com>
2013-12-01 14:09:17 +0700

    penambahan halaman home

---
+++ home.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet/less" type="text/css" href="styles.less"/>
+	<script src="less-1.5.0.min.js" type="text/javascript"></script>
+	<script src="jquery-1.10.2.js" type="text/javascript"></script>
+	<title>Home</title>
+</head>
+<body>
+	<header>
+    	<nav>
+            <ul>
+              <li><a href="#" class="sedang-dibuka">Home</a></li>
+              <li><a href="#">Biodata</a></li>
+              <li><a href="#">Krs</a></li>
+              <li><a href="#">Jadwal</a></li>
+              <li><a href="#">Admin</a></li>
+            </ul>
+            <ul id="logout-box">
+              <li><a href="#" id="username-box">Username</a></li>
+              <li><a href="#">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div id="center">
+        <article id="article-home">
+            <h2>This is Article</h2>
+        </article>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="bootstrap.css">
     <link rel="stylesheet/less" type="text/css" href="styles.less"/>
 	<script src="less-1.5.0.min.js" type="text/javascript"></script>
-	<script src="jquery-1.10.2.js"></script>
+	<script src="jquery-1.10.2.js" type="text/javascript"></script>
     <title>Jadwal</title>
 </head>
 <body>
