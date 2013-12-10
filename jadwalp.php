08667c68e9b7a0b78493ec8c7e2aeb48b6d76e33
Fendy-Kwan <Hyoutora13@gmail.com>
2013-12-10 13:58:12 +0700

    penambahan fitur notifikasi

--- jadwalp.php
+++ jadwalp.php
@@ -12,6 +12,39 @@
     <link rel="stylesheet/less" type="text/css" href="styles.less"/>
 	<script src="less-1.5.0.min.js" type="text/javascript"></script>
 	<script src="jquery-1.10.2.js" type="text/javascript"></script>
+    <script>
+		var pesan_array = ['warning','error'];	 
+		function hilang()
+		{
+				 var h = new Array();
+				 for (i=0; i<pesan_array.length; i++)
+				 {
+						  h[i] = $('.' + pesan_array[i]).outerHeight();
+						  $('.' + pesan_array[i]).css('top', -h[i]);	  
+				 }
+		}
+		
+		function tampil(type)
+		{
+			$('.'+ type +'-trigger').click(function(){
+				  hilang();				  
+				  $('.'+type).animate({top:"0"}, 500);
+			});
+		}
+		
+		$(document).ready(function(){
+				 hilang();
+				 for(var i=0;i<pesan_array.length;i++)
+				 {
+					tampil(pesan_array[i]);
+				 }
+				 
+				 $('.message').click(function(){			  
+						  $(this).animate({top: -$(this).outerHeight()}, 500);
+				  });		 
+				 
+		});       
+	</script>
 	<title>Home</title>
 </head>
 <body>
@@ -37,6 +70,22 @@
                 </ul>
             </nav>
         </header>
+        <div class="error message">
+                 <h3>Notifikasi Error</h3>
+                 <p>Klik untuk hilang</p>
+        
+        </div>
+        
+        <div class="warning message">
+                 <h3>Notifikasi warning</h3>
+                 <p>Klik untuk hilang</p>
+        </div>
+        
+        <ul id="trigger-list">
+                 <li><a href="#" class="error-trigger">Error</a></li>
+                 <li><a href="#" class="warning-trigger">Warning</a></li>
+        </ul>
+
         <div id="center">
             <article id="article-home">
                 <h2>This is Article</h2>
